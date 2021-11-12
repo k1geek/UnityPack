28a3bd93fdd9f2e04a6c87f5b431994456ee3433
k1geek <k1_geek@163.com>
2021-11-12 18:31:58 +0800

    forld

---
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleBuilder
+{
+    [MenuItem("Tools/AssetBundleTool/BuildAsstBUndleAll")]
+    public static void BuildAsstBUndleAll(){
+        Debug.Log(123);
+    }
+}
