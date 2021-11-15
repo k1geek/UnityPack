7c1e31af977a7a0efa967e94a098e8fee5e0d4b7
k1geek <k1_geek@163.com>
2021-11-15 17:44:11 +0800

    pack assetbundle

--- Assets/Editor/AssetBundleBuilder.cs
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -1,12 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
 public class AssetBundleBuilder
 {
-    [MenuItem("Tools/AssetBundleTool/BuildAsstBUndleAll")]
-    public static void BuildAsstBUndleAll(){
-        Debug.Log(123);
+    public static string data = Application.dataPath.Replace("\\", "/");
+    public static string root = data.Substring(0, data.Length - 6);
+    public static string dest = root + "Packed/";
+    // public static string packres = dest + "packres/";
+
+    [MenuItem("Tools/AssetBundleTool/BuildAsstBundleWin")]
+    public static void BuildAsstBundleWin(){
+        build(BuildTarget.StandaloneWindows64);
+    }
+
+    public static void build(BuildTarget target){
+        if (!Directory.Exists(dest))
+            Directory.CreateDirectory(dest);
+
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(dest,
+            BuildAssetBundleOptions.DisableLoadAssetByFileName |
+            BuildAssetBundleOptions.DisableLoadAssetByFileNameWithExtension |
+            BuildAssetBundleOptions.StrictMode |
+            BuildAssetBundleOptions.DeterministicAssetBundle |
+            BuildAssetBundleOptions.ChunkBasedCompression,
+        target);
+
+        if (manifest == null){
+            Debug.LogError("[BuildPipeline.BuildAssetBundles] build AssetBundle failed");
+            throw new Exception("[E]Error:build AssetBundle failed");
+        }else{
+            Debug.Log("pack assetBundle success");
+        }
+    }
+
+    public static void clear(){
+        if (Directory.Exists(dest)){
+            Directory.Delete(dest,true);
+        }
     }
 }
